279e8c1a8f95900f485f6c109f5fca4ae5769910
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-06-16 08:17:03 +0800

    Add unit test

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,33 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by Faiz Mokhtar on 16/06/2018.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+
+    var entryUnderTest: Entry!
+
+    override func setUp() {
+        super.setUp()
+        entryUnderTest = Entry("Title", "Content")
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
+    }
+}
